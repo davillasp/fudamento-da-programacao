2a2afa54a2bbbe04226b844da9ab575eef7323d4
davillasp <158526417+davillasp@users.noreply.github.com>
2024-07-02 21:33:55 -0300

    Add files via upload

---
+++ input&printtf.py
@@ -0,0 +1,24 @@
+# slave! novo app
+#print (' ola, qual é seu Nome:')
+#print.serve para exebir a tela e termos
+ #name = input(' pode digitar seu nome ?:')
+#impult.serve para inserir texto via console
+#print("ola",name ,"seja bem vindo ao nosso app")
+#esse serve para aparecer o nome na exwcução 
+
+#print("\n ola coloque seu nome,cpf,hotmail,cep")
+#NAME = input('qual seu nome?')
+#CPF = input('qual seu cpf?')
+#HOTMAIL = input('qual seu hotmail?')
+#CEP = input('qual seu cep')
+#print('ola',NAME,'seu cpf é',CPF,'e seu hotmail é',HOTMAIL,'e seu cep é',CEP, 'bem vindo ao app')
+
+print ('Preecha as informações abaixo para completar seu cadastro')
+name = str(input('digite seu nome inteiro:'))
+email = str(input('digite seu email:'))
+cpf = int(input('digite seu cpf'))
+cep = int(input('digite seu cep'))
+print('Parabens {} seu cadastro foi realizado com sucesso.\n Aqui estao seus dados \n nome: {}\n email: {}\n cpf: {}\n cep: {}'.format(name,name,email,cpf,cep))
+
+
+
