16b004ca773a30d1be3f03af5aa37d3fdff59238
davillasp <158526417+davillasp@users.noreply.github.com>
2025-04-02 21:44:14 -0300

    Add files via upload

---
+++ dia noite/dianoite.js
@@ -0,0 +1,20 @@
+const btnDarkModeToggle = document.getElementById('btn-dark-mode-toggle') 
+const themeSystem = localStorage.getItem('themeSystem') || 'light'
+
+btnDarkModeToggle.addEventListener('click',() => {
+    let oldTheme = localStorage.getItem('themeSystem') || 'light'
+    let newTheme = oldTheme == "light" ? "dark" : "light"
+    localStorage.setItem('themeSystem',newTheme)
+    defineCurrentTheme(newTheme)
+})
+
+function defineCurrentTheme(theme){
+    document.documentElement.setAttribute("data-theme", theme)
+    if(theme == "light")
+    {
+        btnDarkModeToggle.innerHTML = darkSvg
+        return
+    }
+    btnDarkModeToggle.innerHTML = lightSvg
+}
+defineCurrentTheme(themeSystem)
